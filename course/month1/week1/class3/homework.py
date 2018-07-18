b94e3f6a51ac7b20d58919bb6ff9fdd43cb77eb9
itechspert <issy.airas@gmail.com>
2018-07-18 15:33:02 +0200

    end of class 3 but will need to do the homework and practice python on code academy next

---
+++ course/month1/week1/class3/homework.py
@@ -0,0 +1,7 @@
+#homework.py
+#Full name greeting. Write a program that asks for a
+#person's first name, then middle, and then last.
+#Finally, it should greet the person using their
+#full nameself.
+
+#Bigger, better favourite number. Write a program that

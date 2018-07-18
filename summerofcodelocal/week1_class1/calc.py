2a2414ec9929855bed28e8860d692a15f8fcc249
itechspert <issy.airas@gmail.com>
2018-07-18 12:19:58 +0200

    workin this out eventually

---
+++ summerofcodelocal/week1_class1/calc.py
@@ -0,0 +1,25 @@
+#calculator
+
+print (3+1)
+print (2)
+print (2*2)
+print (5-8)
+print (9/2)
+
+print (5 * (14-2) + -15)
+print (2**8)
+print (1<<9)
+
+#Hours in a year. Minutes in a decade. Age in seconds. How many days does it take for a 32-bit system to timeout, if it has a bug with integer overflow? How about a 64-bit system?
+print "There are"
+print (365 * 24)
+print "hours in a year"
+print "There are"
+print (10 * 365 * 24 * 60)
+print "minutes in a decade"
+print "I am"
+print (23 * 365 * 24 * 60 * 60)
+print "seconds old"
+
+print (10%2)
+print (11%2)

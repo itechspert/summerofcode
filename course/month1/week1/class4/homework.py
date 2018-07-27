409340c7a9da766e2569474aa201a512e736096b
itechspert <issy.airas@gmail.com>
2018-07-27 12:26:34 +0200

    Update changes

--- course/month1/week1/class4/homework.py
+++ course/month1/week1/class4/homework.py
@@ -1,16 +1,16 @@
-# “99 Bottles of Beer on the Wall.” Write a program that prints out the lyrics to that beloved classic, “99 Bottles of Beer on the Wall.”
-'''bottles = 99
+'''# “99 Bottles of Beer on the Wall.” Write a program that prints out the lyrics to that beloved classic, “99 Bottles of Beer on the Wall.”
+bottles = 99
 for b in str(bottles):
     song = "I've got ", str(bottles), " bottles of beer on the wall, ", str(bottles), "bottles of beer. Take one down and pass it around, ", str(bottles - 1), " bottles of beer on the wall."
     print(song)
     bottles = bottles - 1
-''''''
+
 while bottles >= 0:
     song = "I've got ", str(bottles), "bottles of beer on the wall, ", str(bottles), "bottles of beer. Take one down and pass it around, ", str(bottles - 1), "bottles of beer on the wall."
     print(song)
     bottles = bottles - 1
-'''
-'''import random
+
+import random
 text = ""
 
 while text != ("bye"):
@@ -18,20 +18,19 @@ while text != ("bye"):
 
     if str.isupper(text):
         year = random.randint(1930, 1950)
-        print("NO, NOT SINCE", year, ""''')
+        print("NO, NOT SINCE", year, "")
 
 
-'''while text != "bye":
+while text != "bye":
     text = input("say something please ")
     print ("you said " + text + ".")
 
     if text == "bye":
         print ("good bye to you too")
         break
-'''
 
 
-'''Leap years. Write a program that asks for a starting year
+Leap years. Write a program that asks for a starting year
 and an ending year and then puts all the leap years between them
 (and including them, if they are also leap years). Leap years are
 years divisible by 4 (like 1984 and 2004). However, years divisible
@@ -39,5 +38,10 @@ by 100 are not leap years (such as 1800 and 1900) unless they are
 also divisible by 400 (such as 1600 and 2000, which were in fact
 leap years). What a mess!'''
 
-leapyear = 0
-     
+
+start_year = ""
+end_year = ""
+input("Give me a start year")
+start_year = input("Give me a start year")
+input("Give me an end year")
+end_year = input("Give me an end year")
